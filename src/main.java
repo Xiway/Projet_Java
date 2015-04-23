d8902f38ab8ea2ea6d7243651276c16c821284b6
Rama <r.suntharasarma@gmail.com>
2015-04-23 15:52:25 +0200

    main test

--- src/main.java
+++ src/main.java
@@ -1,4 +1,22 @@
 
 public class main {
-
+	static public void main(String [] args){
+		Velo v1 = new Velo(28,02,2015,11,30,100,90, 120);
+//		v1.affiche();
+		
+		Course c1 = new Course(1,03,2015,20,00,10,180, "Montagne", "Pluie");
+//		c1.affiche();
+		
+		Course c2 = new Course(3,05,2015,06,30,20,300,"Plage", "Soleil");
+		 
+		Agenda MyAgenda = new Agenda();
+		MyAgenda.add(c1);
+		MyAgenda.add(v1);
+		MyAgenda.add(c2);
+		
+		MyAgenda.affiche(1);
+		MyAgenda.longueseance().affiche();
+		MyAgenda.courteseance().affiche();
+		MyAgenda.distanceDsplne("course");
+	}
 }
